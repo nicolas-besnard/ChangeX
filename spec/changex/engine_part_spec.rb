653b7ed9b535f29b3377b8c922c02ec584259b10
Nicolas Besnard <besnard.nicolas@gmail.com>
2015-11-24 16:14:34 +0100

    story-02

--- spec/changex/engine_part_spec.rb
+++ spec/changex/engine_part_spec.rb
@@ -16,5 +16,32 @@ describe Changex::EnginePart do
     it 'has a price' do
       expect(@engine_part.price).to eql(@engine_part_price)
     end
+
+    it 'has an empty sub_components array' do
+      expect(@engine_part.sub_components).to eql([])
+    end
+  end
+
+  describe "#<<" do
+    context "object is an EnginePart" do
+      it "add the EnginePart as a sub_component" do
+        engine_part = described_class.new(name: "EnginePartName", price: 10)
+        sub_engine_part = described_class.new(name: "SubEnginePartName", price: 10)
+
+        expect {
+          engine_part << sub_engine_part
+        }.to change(engine_part.sub_components, :count).by(1)
+      end
+    end
+
+    context "object is not an EnginePart" do
+      it "raises an exception" do
+        engine_part = described_class.new(name: "EnginePartName", price: 10)
+
+        expect {
+          engine_part << "Foo"
+        }.to raise_error(Changex::EnginePart::NotAnEnginePart)
+      end
+    end
   end
 end
