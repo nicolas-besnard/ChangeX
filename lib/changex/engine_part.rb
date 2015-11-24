0bdb070405415482c5f11633594c8dd471a7ec83
Nicolas Besnard <besnard.nicolas@gmail.com>
2015-11-24 16:35:25 +0100

    story-04

--- lib/changex/engine_part.rb
+++ lib/changex/engine_part.rb
@@ -21,6 +21,26 @@ module Changex
       end
     end
 
+    def to_s
+      sub_components_count = self.sub_components.count
+
+      if sub_components_count == 0
+        return ''
+      else
+        ret = "The #{self.name} is composed of #{sub_components_count} elements:"
+        self.sub_components.each do |sub_component|
+          ret += "- #{sub_component.name}"
+        end
+        ret += " "
+      end
+
+      self.sub_components.each do |sub_component|
+        ret += sub_component.to_s
+      end
+
+      ret
+    end
+
     NotAnEnginePart = Class.new(StandardError)
   end
 end
